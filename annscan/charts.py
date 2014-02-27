85ff9190466dbe971f200c4c7f9d2f267049be70
marcelo_cure <marcelo.cure@ilegra.com>
2014-02-27 19:31:22 -0300

    changing chart labels and fixes to visualization

--- annscan/charts.py
+++ annscan/charts.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 
 
 def generate_chart(total_files, occurrencies):
-    labels = 'With @Refactoring', 'Without @Refactoring'
+    labels = 'Annotation occurrencies', 'Files without annotation'
     sizes = [total_files, occurrencies]
     colors = ['yellowgreen', 'gold', ]
-    explode = (0, 0.1)
+    explode = (0.2, 0)
 
     plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-            autopct='%1.1f%%', shadow=False, startangle=180)
-    plt.axis('equal')
+            autopct='%1.1f%%', shadow=True, startangle=90)
+    #plt.axis('equal')
     plt.savefig('chart.png')
     plt.show()
