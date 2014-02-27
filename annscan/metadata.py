e94d924125316f9c6fad11a2e6c249e177df9f3d
marcelo_cure <marcelo.cure@ilegra.com>
2014-02-27 19:34:19 -0300

    changes to metadata.py

--- annscan/metadata.py
+++ annscan/metadata.py
@@ -4,14 +4,14 @@ Information describing the project.
 """
 
 # The package name, which is also the "UNIX name" for the project.
-package = 'refscan'
-project = "Refactoring Annotation Scanner"
+package = 'annscan'
+project = "Annotation scanner"
 project_no_spaces = project.replace(' ', '')
 version = '0.1'
-description = 'Annotation scanner under specific extension'
+description = 'Annotation scanner for specific extension'
 authors = ['Marcelo Cure', ]
 authors_string = ', '.join(authors)
 emails = ['marcelocure@gmail.com', ]
 license = 'MIT'
 copyright = '2014 ' + authors_string
-url = 'http://www.ilegra.com/'
+url = 'http://github.com/marcelocure'
