9b716ac31550b4627c4f13bb57d6233729b9ebbf
marcelo_cure <marcelo.cure@ilegra.com>
2014-02-27 19:25:24 -0300

    tests for io_file module

---
+++ tests/io_file_test.py
@@ -0,0 +1,26 @@
+import pytest
+from annscan import io_file
+
+parametrize = pytest.mark.parametrize
+
+
+class Test(object):
+    @parametrize('file_to_read', ['tests/resources/testfileoneline.java'])
+    def test_get_file_content_one_line(self, file_to_read):
+        content = io_file.get_file_content(file_to_read)
+        assert len(content) == 1
+        assert content[0] == 'test file . java'
+
+    @parametrize('file_to_read', ['tests/resources/testfiletwolines.java'])
+    def test_get_file_content_two_lines(self, file_to_read):
+        content = io_file.get_file_content(file_to_read)
+        assert len(content) == 2
+        assert content[0] == 'test file . java\n'
+        assert content[1] == 'second line'
+
+    @parametrize('path', ['tests/resources'])
+    def test_scan_path(self, path):
+        content = io_file.scan_path(path)
+        assert len(content) == 2
+        assert content[0] == 'tests/resources/testfileoneline.java\n'
+        assert content[1] == 'tests/resources/testfiletwolines.java\n'
---
+++ tests/resources/testfiletwolines.java
@@ -0,0 +1,2 @@
+test file . java
+second line
